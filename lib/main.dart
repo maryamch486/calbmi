77c17b7690c6b11c8a92493165139f3963288e7a
maryamch486 <maryamsadiq1616@gmail.com>
2020-11-26 10:03:26 -0300

    primarycolor

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF1F184C),
+        primaryColor: Color(0xFF1F186c),
         scaffoldBackgroundColor: Colors.blue,
       ),
       home: InputPage(),
