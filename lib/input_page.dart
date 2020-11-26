e017d564b68120329bf42a6bf56c3d1764d2d467
maryamch486 <maryamsadiq1616@gmail.com>
2020-11-26 10:00:03 -0300

    margin

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -205,8 +205,8 @@ class _InputPageState extends State<InputPage> {
                   MaterialPageRoute(builder: (context) => ResultPage()));
             },
             child: Container(
-              margin: EdgeInsets.only(top: 10.0),
-              padding: EdgeInsets.only(bottom: 20.0),
+              margin: EdgeInsets.only(top: 8.0),
+              padding: EdgeInsets.only(bottom: 15.0),
               width: double.infinity,
               height: 70.0,
               color: Colors.lightGreen,
